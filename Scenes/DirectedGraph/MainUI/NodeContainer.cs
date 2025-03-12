88947b2a0b1fec33d6cba1fc123f22d19cef5c62
Arnob672 <80312770+Arnob90@users.noreply.github.com>
2025-03-12 23:11:26 +0600

    Added UIDs from Godot 4.4

---
+++ Scenes/DirectedGraph/MainUI/NodeContainer.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using RelationSpace;
+public partial class NodeContainer : Node2D
+{
+    public DirectedGraph<T> CreateGraphFromRelation()
+    {
+    }
+}
